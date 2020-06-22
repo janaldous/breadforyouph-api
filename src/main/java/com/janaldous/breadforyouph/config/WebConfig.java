77937a8e0872ee7d48234355dad8255e14b1c2eb
janaldoustorres <jat.torres@gmail.com>
2020-06-23 00:40:30 +0800

    added domain to cors

--- src/main/java/com/janaldous/breadforyouph/config/WebConfig.java
+++ src/main/java/com/janaldous/breadforyouph/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.janaldous.breadforyouph.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,14 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
+	@Value("${breadforyouph.api.domain}")
+	private String breadforyouphApiDomain; 
+
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000", breadforyouphApiDomain);
 			}
 		};
 	}
-	
+
 }
