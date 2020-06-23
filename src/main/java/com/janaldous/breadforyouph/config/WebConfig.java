96afb4ed1d0e709d34bb902e9897866574cc81ae
janaldoustorres <jat.torres@gmail.com>
2020-06-23 22:34:37 +0800

    added dev api to cors

--- src/main/java/com/janaldous/breadforyouph/config/WebConfig.java
+++ src/main/java/com/janaldous/breadforyouph/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000", breadforyouphApiDomain);
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000", "https://breadforyouph-dev.herokuapp.com", breadforyouphApiDomain);
 			}
 		};
 	}
